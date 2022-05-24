5d21cc3ec11586211ed334f32d74bd422572da4c
maarirods <rodspersonal22@gmail.com>
2022-05-24 16:33:26 -0300

    aula 17 try catch excecao

--- src/test/java/br/com/chronosAcademy/pages/LoginPage.java
+++ src/test/java/br/com/chronosAcademy/pages/LoginPage.java
@@ -7,44 +7,55 @@ import org.openqa.selenium.support.PageFactory;
 public class LoginPage {
     LoginMap loginMap;
 
-    public LoginPage(){
-        loginMap= new LoginMap();
+
+    public LoginPage() {
+        loginMap = new LoginMap();
         PageFactory.initElements(Driver.getDriver(), loginMap);
     }
 
-    public void clickBtnLogin(){
+    public void clickBtnLogin() {
+        Driver.visibilityOf(loginMap.btnLogin);
         loginMap.btnLogin.click();
-    }
-
-    public void clickDivFecharModal(){
-        loginMap.divFecharModal.click();
-    }
-
-    public void setInpUserName(String username){
-        loginMap.inpUserName.sendKeys(username);
-    }
-
-    public void setInpPassword(String password){
-        loginMap.inpPassword.sendKeys(password);
-    }
-
-    public void clickInpRemember(){
-        loginMap.inpRemember.click();
-    }
-
-    public void clickLinkCreateAccount(){
-        loginMap.linkCreateAccount.click();
-    }
-
-    public void clickBtnSignIn(){
-        loginMap.btnSignInp.click();
-    }
-
-    public boolean isBtnSignIn(){
-        return  loginMap.btnSignInp.isEnabled();
     }
 
     public void clickBtnFechar() {
         loginMap.btnFechar.click();
     }
+
+    public void clickDivFecharModal() {
+        loginMap.divFecharModal.click();
+    }
+
+    public void setInpUserName(String username) {
+        loginMap.inpUserName.sendKeys(username);
+    }
+
+    public void setInpPassword(String password) {
+        loginMap.inpPassword.sendKeys(password);
+    }
+
+    public void clickInpRemember() {
+        loginMap.inpRemember.click();
+    }
+
+    public void clickLinkCreateAccount() {
+        loginMap.linkCreateAccount.click();
+    }
+
+    public void clickBtnSignIn() {
+        loginMap.btnSignInp.click();
+    }
+
+    public boolean isBtnSignIn() {
+        return loginMap.btnSignInp.isEnabled();
+    }
+
+    public void visibilityOfBtnFechar(){
+        Driver.visibilityOf(loginMap.btnFechar);
+    }
+
+    public void invisibilityOfBtnFechar(){
+        Driver.invisibilityOf(loginMap.btnFechar);
+    }
+
 }
