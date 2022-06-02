0ea5c9464c073b92a0127619df77057029a337fb
maarirods <rodspersonal22@gmail.com>
2022-06-02 18:01:22 -0300

    aula 20 massa de dados

--- src/test/java/br/com/chronosAcademy/runner/RunnerTest.java
+++ src/test/java/br/com/chronosAcademy/runner/RunnerTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        tags = "@dadosEmBranco",
+        tags = "@loginInvalido",
         glue = "br.com.chronosAcademy.steps",
         plugin = {"json:target/reports/"},
         snippets = CucumberOptions.SnippetType.CAMELCASE
